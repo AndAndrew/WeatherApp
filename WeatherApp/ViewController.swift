f1ce6fc3fc22147a7dc137b9471f3a1bdf757008
AndAndrew <andrew.krivokhizhin@gmail.com>
2020-05-09 16:17:18 +0400

    View

--- WeatherApp/ViewController.swift
+++ WeatherApp/ViewController.swift
@@ -9,7 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+// api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={your api key}
+    
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var pressureLabel: UILabel!
+    @IBOutlet weak var humidityLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var appearentTemperatureLabel: UILabel!
+    @IBOutlet weak var refreshButton: UIButton!
+    @IBAction func refreshButtonTapped(_ sender: UIButton) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
